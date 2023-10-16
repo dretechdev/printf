348d448e53f44bfc7a7a1a9d04757fb5d0a8ab37
OusmaneMano <ousmanemanot@gmail.com>
2023-10-15 17:37:49 -0700

    Correct printf file

--- _printf.c
+++ _printf.c
@@ -8,34 +8,40 @@ int _printf(const char *format, ...)
 {
 	unsigned int i, leng = 0, leng2 = 0;
 	va_list list;
+	char ch;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(list, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			put_char(format[i]);
 		}
-		else if (format[i + 1] == 'c')
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			if (format[i] == '%' && format[i + 1] == ch)
+			{
+				put_char('%');
+			}
+		if (format[i] == '%' && format[i + 1] == 'c')
 		{
 			put_char(va_arg(list, int));
 			i++;
 		}
-		else if (format[i + 1] == 's')
+		else if (format[i] == '%' && format[i + 1] == 's')
 		{
 			leng2 = put_c(va_arg(list, char *));
 			i++;
 			leng += (leng2 - 1);
 		}
-		else if (format[i + 1] == '%')
+		}
+		if (format[i] == '%' && format[i + 1] == '%')
 		{
 			put_char('%');
 		}
-
 		leng += 1;
 	}
 	va_end(list);
